98b9adaaeba79fd7c85fd33e4683de3e1466d633
KattaHarshavardhanReddy <katta.harshareddy@gmail.com>
2025-06-20 12:39:00 +0530

    Conv b/w me n janith

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,9 @@
 #!bin/bash/
 
-echo Harsha: Hi Janith
-echo Janith: Hello DAD!
-echo Harsha: Howare you?
-echo Janith: I Am good
+N1 = Harsha
+N2 = Janith
+
+echo $N1: Hi $N2
+echo $N2: Hello DAD!
+echo $N1: Howare you?
+echo $N2: I Am good
