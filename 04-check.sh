e61d55f195bb06656a96bad82b79a4cbb2fb7c10
KattaHarshavardhanReddy <katta.harshareddy@gmail.com>
2025-06-21 12:46:25 +0530

    hi

--- 04-check.sh
+++ 04-check.sh
@@ -7,5 +7,15 @@ then
 echo "ur not root"
 exit 1
 fi
+
 dnf install mysqll -y
+
+if [$? -ne 0 ]
+then
+echo "mysql installation is failed"
+exit 1
+else
+echo "mysql installed"
+fi
+
 dnf install git -y
